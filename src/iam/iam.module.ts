260ed2f588758c737a2285bc0b0f2ce4bda52a37
Vlad Sh <kausvirtual@gmail.com>
2023-08-31 01:09:39 +0300

    added access-token.guard

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -7,6 +7,8 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
 import jwtConfig from './config/jwt.config';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './authentication/guards/access-token.guard';
 
 @Module({
   imports: [
@@ -18,6 +20,10 @@ import { ConfigModule } from '@nestjs/config';
     {
       provide: HashingService,
       useClass: BcryptService,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
     },
     AuthenticationService,
   ],
