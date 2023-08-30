0395ceb7579fb842a74487f2763458e823d4f0cc
Vlad Sh <kausvirtual@gmail.com>
2023-08-30 18:12:44 +0300

    added authentication.controller

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
+import { SignUpDto } from './dto/sign-up.dto';
 
 @Controller('authentication')
 export class AuthenticationController {
   constructor(private readonly authService: AuthenticationService) {}
+
+  @Post('singUp')
+  singUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.singUp(signUpDto);
+  }
+
+  @Post('signIn')
+  signIn(@Body() signInDto: SignUpDto) {
+    return this.authService.signIn(signInDto);
+  }
 }
