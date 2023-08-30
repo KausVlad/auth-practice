39e176a76c76107885400f6436d0c5ee6b833577
Vlad Sh <kausvirtual@gmail.com>
2023-08-30 20:04:16 +0300

    revers out from save in cookie

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,14 +1,6 @@
-import {
-  Body,
-  Controller,
-  HttpCode,
-  HttpStatus,
-  Post,
-  Res,
-} from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { SignUpDto } from './dto/sign-up.dto';
-import { Response } from 'express';
 
 @Controller('authentication')
 export class AuthenticationController {
@@ -21,16 +13,22 @@ export class AuthenticationController {
 
   @HttpCode(HttpStatus.OK)
   @Post('signIn')
-  async signIn(
-    @Res({ passthrough: true }) response: Response,
-    @Body() signInDto: SignUpDto,
-  ) {
-    const accessToken = await this.authService.signIn(signInDto);
-    console.log(accessToken);
-    response.cookie('accessToken', accessToken, {
-      // secure: true,
-      httpOnly: true,
-      sameSite: true,
-    });
+  signIn(@Body() signInDto: SignUpDto) {
+    return this.authService.signIn(signInDto);
   }
+
+  // @HttpCode(HttpStatus.OK)
+  // @Post('signIn')
+  // async signIn(
+  //   @Res({ passthrough: true }) response: Response,
+  //   @Body() signInDto: SignUpDto,
+  // ) {
+  //   const accessToken = await this.authService.signIn(signInDto);
+  //   console.log(accessToken);
+  //   response.cookie('accessToken', accessToken, {
+  //     // secure: true,
+  //     httpOnly: true,
+  //     sameSite: true,
+  //   });
+  // }
 }
