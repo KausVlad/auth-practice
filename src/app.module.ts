1753b2f7e236f12a9f4d4d49b94c723b31e8ed73
Vlad Sh <kausvirtual@gmail.com>
2023-08-30 16:27:11 +0300

    Identity and Access Management

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { IamModule } from './iam/iam.module';
 
 @Module({
-  imports: [CoffeesModule, UsersModule, PrismaModule],
+  imports: [CoffeesModule, UsersModule, PrismaModule, IamModule],
   controllers: [AppController],
   providers: [AppService],
 })
